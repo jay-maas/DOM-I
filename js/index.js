f97955957b33d6ee71329af970ec3b7c614f7c48
Jay Maas <jaymaas.business@gmail.com>
2019-04-08 15:45:39 -0600

    DOM Main Content/Contact
    completed the main content and contact

--- js/index.js
+++ js/index.js
@@ -42,6 +42,8 @@ let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 let cta_img = document.getElementById("cta-img");
 cta_img.setAttribute('src', siteContent["cta"]["img-src"])
+let middle_img = document.getElementById("middle-img");
+middle_img.setAttribute('src', siteContent["main-content"]["middle-img-src"])
 
 //Nav
 let a = document.querySelectorAll("nav a");
@@ -58,5 +60,26 @@ h1.innerHTML = siteContent["cta"]["h1"]
 let button = document.querySelector(".cta button");
 button.innerHTML = siteContent["cta"]["button"]
 
+//main-content
+let main_content_h4 = document.querySelectorAll(".main-content h4");
+main_content_h4[0].innerHTML = siteContent["main-content"]["features-h4"]
+main_content_h4[1].innerHTML = siteContent["main-content"]["about-h4"]
+main_content_h4[2].innerHTML = siteContent["main-content"]["services-h4"]
+main_content_h4[3].innerHTML = siteContent["main-content"]["product-h4"]
+main_content_h4[4].innerHTML = siteContent["main-content"]["vision-h4"]
 
+let main_content_p = document.querySelectorAll(".main-content p");
+main_content_p[0].innerHTML = siteContent["main-content"]["features-content"]
+main_content_p[1].innerHTML = siteContent["main-content"]["about-content"]
+main_content_p[2].innerHTML = siteContent["main-content"]["services-content"]
+main_content_p[3].innerHTML = siteContent["main-content"]["product-content"]
+main_content_p[4].innerHTML = siteContent["main-content"]["vision-content"]
 
+//Contact
+let contact_h4 = document.querySelector(".contact h4");
+contact_h4.innerHTML = siteContent["contact"]["contact-h4"]
+
+let contact_p = document.querySelectorAll(".contact p");
+contact_p[0].innerHTML = siteContent["contact"]["address"]
+contact_p[1].innerHTML = siteContent["contact"]["phone"]
+contact_p[2].innerHTML = siteContent["contact"]["email"]
